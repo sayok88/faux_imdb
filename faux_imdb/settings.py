25d06d1c09ec1ca0a5191c9ca88da27dded5d821
sayok88 <namesake88@gmail.com>
2018-06-24 20:13:21 +0530

    abc

--- faux_imdb/settings.py
+++ faux_imdb/settings.py
@@ -78,13 +78,17 @@ WSGI_APPLICATION = 'faux_imdb.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'faux_imdb_db',
+        'USER': 'name',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
